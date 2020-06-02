6a3d5d939853c4ea75fb7f8c1b45b2d2d546379f
Valentin68 <valentin.mercy.vm@gmail.com>
2020-06-02 20:25:58 +0200

    added shebang

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
+#! /bin/bash
 #TODO: Install new I2C interface
 #TODO: Install systemctl services, reload daemon
 python setup.py install
 echo "Installation terminee ! Redemarrez la Raspberry Pi pour commencer a utiliser le boitier de controle."
 read -p "Redemarrer maintenant ? (O/n)" -n 1 -r
-echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Oo]$ ]]
 then
     reboot now
--- setup.py
+++ setup.py
@@ -61,7 +61,7 @@ setup(
     # project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['configparser','treelib', 'spidev', 'psutil','pyudev','requests','urllib','shutil'],
+    install_requires=['configparser','treelib', 'spidev', 'psutil','pyudev','requests','shutils'],
 
     # List additional groups of dependencies here (e.g. development dependencies).
     # You can install these using the following syntax, for example:
