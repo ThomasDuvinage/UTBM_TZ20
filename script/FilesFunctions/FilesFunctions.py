ef8a9fec6d6e38bc69f54e1932e9b4ea425646a9
Valentin Mercy <mercy.valentin@gmail.com>
2021-10-23 13:06:43 +0200

    test

--- script/FilesFunctions/FilesFunctions.py
+++ script/FilesFunctions/FilesFunctions.py
@@ -554,7 +554,7 @@ class Files():
             print("PATH DSI FILE : ",self.pathDSIFile)
             self.os.system("cp "+self.pathDSIFile + " " +
                            self.folderPathName + '/absents.csv')
-            self.absents = self.__row_count(self.pathDSIFile)
+            self.absents = self.__row_count(self.pathDSIFile+'/absents.csv')
 
         with open(self.folderPathName + '/total.csv', self.append) as total_File:
             totalFileWriter = self.csv.writer(
