c46d0afb57a00723e9f110ca1390f52b4610fa33
thomas.duvinage <43036801+totordudu@users.noreply.github.com>
2021-10-26 21:28:28 +0200

    update led pin

--- script/external_devices/rgb_led.py
+++ script/external_devices/rgb_led.py
@@ -1,25 +1,27 @@
 #!/usr/bin/env python
 import RPi.GPIO as GPIO
 
-#define colors by using dictonnary (R,G,B)
+# define colors by using dictonnary (R,G,B)
 colorPanel = {}
-colorPanel['r']=(1,0,0)
-colorPanel['g']=(0,1,0)
-colorPanel['b']=(0,0,1)
-colorPanel['c']=(0,1,1)
-colorPanel['m']=(1,0,1)
-colorPanel['y']=(1,1,0)
-colorPanel['w']=(1,1,1)
+colorPanel['r'] = (1, 0, 0)
+colorPanel['g'] = (0, 1, 0)
+colorPanel['b'] = (0, 0, 1)
+colorPanel['c'] = (0, 1, 1)
+colorPanel['m'] = (1, 0, 1)
+colorPanel['y'] = (1, 1, 0)
+colorPanel['w'] = (1, 1, 1)
 
 """
     This class represents the Led 
 """
+
+
 class Led():
     # Constructor
     def __init__(self):
         self.r = 21
-        self.g = 20
-        self.b = 16
+        self.g = 16
+        self.b = 20
 
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(self.r, GPIO.OUT, initial=GPIO.HIGH)
@@ -32,15 +34,16 @@ class Led():
             This method is used to set the color of the led by passing in argument an array of color state
         """
         if color in colorPanel:
-            GPIO.output(self.r,1-colorPanel[color][0])
-            GPIO.output(self.g,1-colorPanel[color][1])
-            GPIO.output(self.b,1-colorPanel[color][2])
-    
+            GPIO.output(self.r, 1-colorPanel[color][0])
+            GPIO.output(self.g, 1-colorPanel[color][1])
+            GPIO.output(self.b, 1-colorPanel[color][2])
+
     # Shutdown method is used to switch off the led
     def shutDown(self):
-            GPIO.output(self.r,1)
-            GPIO.output(self.g,1)
-            GPIO.output(self.b,1)
+        GPIO.output(self.r, 1)
+        GPIO.output(self.g, 1)
+        GPIO.output(self.b, 1)
+
 
 if __name__ == '__main__':
     try:
